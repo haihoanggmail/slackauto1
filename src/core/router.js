e98343e27e6074faee5224216a720641e2ec67d5
haihoanggmail <haihoang.hch@gmail.com>
2026-03-02 11:29:14 +0700

    Update router.js

--- src/core/router.js
+++ src/core/router.js
@@ -1,1 +1,17 @@
+import { handleCommand } from "./command.handler";
+import { handleAction } from "./action.handler";
 
+export async function router(body, env) {
+
+  // Slash command
+  if (body.command) {
+    return await handleCommand(body, env);
+  }
+
+  // Button action
+  if (body.type === "block_actions") {
+    return await handleAction(body, env);
+  }
+
+  return { text: "Unknown request" };
+}
