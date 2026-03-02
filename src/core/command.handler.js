dbdedf7e0dfb912d4dca2462952e9e8245eb6f21
haihoanggmail <haihoang.hch@gmail.com>
2026-03-02 11:29:29 +0700

    Update command.handler.js

--- src/core/command.handler.js
+++ src/core/command.handler.js
@@ -1,1 +1,18 @@
+export async function handleCommand(body, env) {
+  const { command, text, user_id } = body;
 
+  switch (command) {
+
+    case "/invoice":
+      return {
+        response_type: "ephemeral",
+        text: `Invoice command received from <@${user_id}>`
+      };
+
+    default:
+      return {
+        response_type: "ephemeral",
+        text: "Unknown command"
+      };
+  }
+}
