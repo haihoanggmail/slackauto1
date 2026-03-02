273749d578fba091e0236514621a428c08961e39
haihoanggmail <haihoang.hch@gmail.com>
2026-03-02 11:29:43 +0700

    Update action.handler.js

--- src/core/action.handler.js
+++ src/core/action.handler.js
@@ -1,1 +1,19 @@
+import confirm from "./actions/confirm";
+import cancel from "./actions/cancel";
 
+export async function handleAction(body, env) {
+
+  const actionId = body.actions?.[0]?.action_id;
+
+  switch (actionId) {
+
+    case "confirm":
+      return await confirm(body, env);
+
+    case "cancel":
+      return await cancel(body, env);
+
+    default:
+      return { text: "Unknown action" };
+  }
+}
